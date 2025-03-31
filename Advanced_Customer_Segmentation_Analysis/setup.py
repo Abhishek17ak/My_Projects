1cec049cf7e7dd4de09a1770e9b2eafd1a9459d5
Abhishek Kalugade <89681877+Abhishek17ak@users.noreply.github.com>
2025-03-31 17:29:37 -0400

    uploaded this project

---
+++ Advanced_Customer_Segmentation_Analysis/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read().splitlines()
+
+setup(
+    name="customer_segmentation",
+    version="0.1.0",
+    author="Abhishek Kalugade",
+    author_email="abhishek.kalugade17@gmail.com",
+    description="Advanced customer segmentation analysis using RFM and clustering techniques",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+    ],
+    python_requires=">=3.8",
+    install_requires=requirements,
+) 
